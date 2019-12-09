d355910e6c41fd5243d1a9a4e4bd9bbcc65fe85f
JOHN NELSON <JohnNelson4850@gmail.com>
2019-12-09 13:54:28 -0800

    Homes routes are tested

--- e2e/__tests__/Homes.test.js
+++ e2e/__tests__/Homes.test.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const connect = require('../../lib/utils/connect');
 const User = require('../../lib/model/User');
 const agent = request.agent(app);
+const Home = require('../../lib/model/Home');
 
 
 describe('Tests the Homes route', () => {
@@ -38,6 +39,66 @@ describe('Tests the Homes route', () => {
           title: 'Test House',
           user: expect.any(String)
         });
+      });
+  });
+
+  it('Should get all homes for a user', async() => {
+    const user1 = await User.create({ username: 'test', password: '1234' });
+    const homes = await Home.create([{ title: 'Test House', user: user1._id }, { title: 'Test House2', user: user1._id }]);
+    await agent
+      .post('/api/v1/auth/signin')
+      .send({ username: 'test', password: '1234' });
+  
+    return agent
+      .get(`/api/v1/homes/user/${user1._id}`)
+      .expect(200)
+      .then(res => {
+        const parsedHomes = JSON.parse(JSON.stringify(homes));
+        expect(res.body).toHaveLength(2);   
+        parsedHomes.forEach(home => {
+          expect(res.body).toContainEqual(home);
+        });
+      });
+  });
+
+  it('Should delete a Home', async() => {
+    const user1 = await User.create({ username: 'test', password: '1234' });
+    const homes = await Home.create([{ title: 'Test House', user: user1._id }, { title: 'Test House2', user: user1._id }]);
+    await agent
+      .post('/api/v1/auth/signin')
+      .send({ username: 'test', password: '1234' });
+    return agent
+      .delete(`/api/v1/homes/${homes[0]._id}`)
+      .then(res => {
+        expect(res.body).toEqual(JSON.parse(JSON.stringify(homes[0])));
+      });
+  });
+
+  it('Should Update a Home', async() => {
+    const user1 = await User.create({ username: 'test', password: '1234' });
+    const homes = await Home.create([{ title: 'Test House', user: user1._id }, { title: 'Test House2', user: user1._id }]);
+    await agent
+      .post('/api/v1/auth/signin')
+      .send({ username: 'test', password: '1234' });
+    return agent
+      .put(`/api/v1/homes/${homes[0]._id}`)
+      .send({ title: 'Updated Title' })
+      .then(res => {
+        expect(res.body.title).toEqual('Updated Title');
+      });
+  });
+
+  it('Should get one home from its Id', async() => {
+    const user1 = await User.create({ username: 'test', password: '1234' });
+    const homes = await Home.create([{ title: 'Test House', user: user1._id }, { title: 'Test House2', user: user1._id }]);
+    await agent
+      .post('/api/v1/auth/signin')
+      .send({ username: 'test', password: '1234' });
+    return agent
+      .get(`/api/v1/homes/${homes[0]._id}`)
+      .expect(200)
+      .then(res => {
+        expect(res.body._id).toEqual(JSON.parse(JSON.stringify(homes[0]._id)));
       });
   });
 
